897e2ec0d73f5ae519bf7df946143b36e856cc8d
redtable <wangsen8@xdf.cn>
2021-04-07 18:23:54 +0800

    create file

---
+++ NEPlayerView/Classes/NEPlayerCoreProtocol.h
@@ -0,0 +1,16 @@
+//
+//  NEPlayerCoreProtocol.h
+//  NEPlayerView
+//
+//  Created by WangSen on 2021/4/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol NEPlayerCoreProtocol <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
